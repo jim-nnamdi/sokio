2150a326b1e906641e9388520f81d58868a60406
Jim <jimsamuel50@gmail.com>
2025-08-28 22:11:19 +0100

    remove bind & listen for broadcast peer and set socket option for broadcast level

--- src/core/discover.rs
+++ src/core/discover.rs
@@ -1,32 +1,29 @@
 use std::{io, mem, net::Ipv4Addr};
 
-/**
- * @brief
- * broadcast signal from a peer to enable lurking
- * peers to track & sync accordingly
- */
 pub fn _broadcast(port: u16) -> io::Result<()> {
     unsafe {
-        let mut adr :libc::sockaddr_in =  mem::zeroed();
-        let adl = mem::size_of::<libc::sockaddr>() as libc::socklen_t;
+        let mut adr: libc::sockaddr_in = mem::zeroed();
         let cfd = libc::socket(libc::AF_INET, libc::SOCK_DGRAM, 0);
-        if cfd < 0 { return Err(io::Error::last_os_error());}
+        if cfd < 0 {
+            return Err(io::Error::last_os_error());
+        }
+
+        let cast:libc::c_int = 1;
+        libc::setsockopt(cfd, libc::SOL_SOCKET, libc::SO_BROADCAST, &cast as *const _ as *const libc::c_void, mem::size_of_val(&cast) as libc::socklen_t);
 
         adr.sin_family = libc::AF_INET as u8;
         adr.sin_port = port.to_be();
-        adr.sin_addr.s_addr = u32::from(Ipv4Addr::new(255,255,255, 255));
+        adr.sin_addr.s_addr = u32::from(Ipv4Addr::new(255, 255, 255, 255)).to_be();
         adr.sin_len = mem::size_of::<libc::sockaddr_in>() as u8;
-        adr.sin_zero = [0;8];
+        adr.sin_zero = [0; 8];
 
-        let cbd = libc::bind(cfd , &mut adr as *mut _ as  *mut libc::sockaddr, adl);
-        if cbd < 0 {return Err(io::Error::last_os_error());}
-        if libc::listen(cfd, 10) < 0  {return Err(io::Error::last_os_error())}
-        loop {
-            /* sendto & recv from calls for UDP broadcasting */
+        let msg = b"peer broadcast";
+        let bcast = libc::sendto(cfd, &msg as *const _ as *const libc::c_void, msg.len(), 0, &adr as *const _  as *const libc::sockaddr, mem::size_of::<libc::sockaddr>() as libc::socklen_t);
+        if bcast < 0 {
+            return Err(io::Error::last_os_error());
         }
+        Ok(())
     }
 }
 
-pub fn _listen_for_peers() {
-
-}
+pub fn _listen_for_peers() {}
